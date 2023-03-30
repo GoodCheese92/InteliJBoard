f8ded65418616534fe54864ca2b1afa2daf00bae
GoodCheese92 <dltjdwls555@gmail.com>
2023-03-30 10:54:10 +0900

    테스트

--- Board_bootstrap/Board/src/vo/BoardVO.java
+++ Board_bootstrap/Board/src/vo/BoardVO.java
@@ -3,7 +3,8 @@ package vo;
 public class BoardVO {
     private int idx, readhit, ref, step, depth, del_info;
     private String name, subject, content, pwd, ip, regidate;
-
+    // 깃허브 테스트 중
+    
     public int getIdx() {
         return idx;
     }
